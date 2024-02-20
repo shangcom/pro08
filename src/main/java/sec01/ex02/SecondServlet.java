e3e015aec09bae104ba3f34df501f014ea5afd72
shangcom <dndbs91@gmail.com>
2024-02-20 16:46:31 +0900

    서블릿 포워드 방법

---
+++ src/main/java/sec01/ex02/SecondServlet.java
@@ -0,0 +1,24 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><body>");
+		pw.println("refresh를 이용한 redirect 실습입니다.");
+		pw.println("</body></html>");
+	}
+
+}
